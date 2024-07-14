5fd756259f73a7f97d4ff94e5e080c87c5459e1f
shubham kumar <shubhofficial1@gmail.com>
2024-07-15 01:35:20 +0530

    Added CallToAction Animation

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,21 +1,38 @@
+"use client";
 import helixImage from "../assets/images/helix2.png";
 import emojiStarImage from "../assets/images/emojistar.png";
 import Image from "next/image";
+import { useScroll, useTransform, motion } from "framer-motion";
+import { useRef } from "react";
 
 const CallToAction = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start end", "end end"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [50, -50]);
+
   return (
-    <div className="bg-black text-white py-[72px] sm:py-24 text-center">
+    <div
+      className="bg-black text-white py-[72px] sm:py-24 text-center"
+      ref={containerRef}
+    >
       <div className="container max-w-xl relative ">
-        <Image
-          src={helixImage}
-          alt="helixImage"
-          className="hidden lg:flex lg:absolute top-6 left-[calc(100%-16px)]"
-        />
-        <Image
-          src={emojiStarImage}
-          alt="emojiStarImage"
-          className="hidden lg:flex lg:absolute -top-[120px] right-[calc(100%-24px)] "
-        />
+        <motion.div style={{ translateY }}>
+          <Image
+            src={helixImage}
+            alt="helixImage"
+            className="hidden lg:flex lg:absolute top-6 left-[calc(100%-6px)]"
+          />
+        </motion.div>
+        <motion.div style={{ translateY }}>
+          <Image
+            src={emojiStarImage}
+            alt="emojiStarImage"
+            className="hidden lg:flex lg:absolute -top-[120px] right-[calc(100%-4px)] "
+          />
+        </motion.div>
         <h2 className="text-5xl sm:text-6xl font-bold tracking-tighter ">
           Get Instant Access
         </h2>
